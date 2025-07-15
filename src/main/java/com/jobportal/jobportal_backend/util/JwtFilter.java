6ae90dead7c952557b981a20107f63c0be35294c
pirangicharan <ramcharan.psb@gmail.com>
2025-07-16 00:35:44 +0530

    cleanup | Ramcharan

--- src/main/java/com/jobportal/jobportal_backend/util/JwtFilter.java
+++ src/main/java/com/jobportal/jobportal_backend/util/JwtFilter.java
@@ -14,7 +14,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
